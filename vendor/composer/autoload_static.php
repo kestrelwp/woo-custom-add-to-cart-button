08c532cdac90ae6b31355c3323c11704e507895e
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2025-03-24 12:32:08 +0800

    Commit composer lock file

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -25,11 +25,16 @@ class ComposerStaticInitd3b56662dd42fb73dc0626fbc3471339
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitd3b56662dd42fb73dc0626fbc3471339::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitd3b56662dd42fb73dc0626fbc3471339::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitd3b56662dd42fb73dc0626fbc3471339::$classMap;
 
         }, null, ClassLoader::class);
     }
